8139e7f17f3c0313fbb9dfe7b881d91ee6074a44
bellco <bellco579@gmail.com>
2018-06-11 18:02:47 +0500

    before begin working

--- order/urls.py
+++ order/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	path('every_order/$',views.every_order,name = 'every_order'),
 	path('new/$',views.new_order,name = 'new_order'),
-	path('show/(?<order_id>)/$',views.show_offer,name = 'show_offer'),
+	path('show/(?<order_id>/$',views.show_offer,name = 'show_offer'),
 	path('personal_page/(?<order_id>/$',views.order_pp,name = 'order_pp'),
     # path('bascket_riew/$',views.bascket_riew, name = 'bascket_riew'),
     # # path('/(?<goods_id>/$',views.GoodsPage, name = 'Goodspage'),
